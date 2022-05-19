df9118445dd99a6a8bd2b76d0b0e68189048bfa0
Sameer1324 <105704163+Sameer1324@users.noreply.github.com>
2022-05-19 13:26:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var weight = [30, 50, 40, 60]
+var sum = weight[0] + weight[1] + weight[2] + weight[3]
+var average = sum / weight.length
+console.log(average)
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(51);
+
+}
+
